0c9c27554fd86fb41c53cdf86511cdc4b9e1cb3a
Alex Milanů <alexumilan@gmail.com>
2024-07-20 19:49:12 +0200

    NPC interaction - quick update Imported TMPro

--- Assets/Scripts/NPC Man/NPCInteract.cs
+++ Assets/Scripts/NPC Man/NPCInteract.cs
@@ -1,44 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NPCInteract : MonoBehaviour
 {
     [SerializeField] private GameObject interactionBox;
+    [SerializeField] private TextMeshProUGUI interactionText;
+
     private bool inRadius = false;
 
 
     private void OnEnable()
     {
-        InputManager.instance.OnInteractInput += InteractNPC;
+        InputManager.InputInstance.OnInteractInput += InteractNPC;
+    }
+
+    private void Start()
+    {
+        interactionText.text = $"Press {InputManager.InputInstance.PlayerInputActions.Player.Interact.GetBindingDisplayString()} to Talk";
     }
 
     private void InteractNPC()
     {
-        if (inRadius)
-        {
-            DialogueManager.Instance.StartDialogue();
-        }
+        if (!inRadius) return;
+
+        DialogueManager.Instance.StartDialogue();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {
-            DialogueManager.Instance.NPC = this;
-            inRadius = true;
-            interactionBox.SetActive(true);
-        }
+        if (!other.CompareTag("Player")) return;
+
+        DialogueManager.Instance.NPC = this;
+        inRadius = true;
+        interactionBox.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {
-            inRadius = false;
-            DialogueManager.Instance.NPC = null;
-            DialogueManager.Instance.EndDialogue();
-            interactionBox.SetActive(false);
-        }
+        if (other.CompareTag("Player")) return;
+
+        inRadius = false;
+        DialogueManager.Instance.NPC = null;
+        DialogueManager.Instance.EndDialogue();
+        interactionBox.SetActive(false);
     }
 }
