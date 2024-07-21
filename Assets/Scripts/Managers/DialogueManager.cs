ca188a3d90390cf014ff2efb731149866ed0d124
Casey Mak <casblood18@gmail.com>
2024-07-20 21:26:24 -0400

    MergeConflict
    Added NPC man fully with interaction(F) and dialogue system, but needs UI visual

--- Assets/Scripts/Managers/DialogueManager.cs
+++ Assets/Scripts/Managers/DialogueManager.cs
@@ -1,28 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class DialogueManager : Singleton<DialogueManager>
 {
+    [SerializeField] private TextMeshProUGUI npcName;
+    [SerializeField] private TextMeshProUGUI npcDialogue;
+    [SerializeField] private GameObject dialoguePanel;
     public NPCInteract NPC { get; set; }
 
     private bool dialogueStarted = false;
+    private Queue<string> dialogueQueue = new Queue<string>();
 
     protected override void Awake()
     {
         base.Awake();
     }
+    private void OnEnable()
+    {
+        InputManager.InputInstance.OnInteractInput += ContinueDialogue;
+    }
+    private void OnDisable()
+    {
+        InputManager.InputInstance.OnInteractInput -= ContinueDialogue;
+    }
 
     public void StartDialogue()
     {
         if (dialogueStarted) return;
+        dialoguePanel.SetActive(true);
         dialogueStarted = true;
-        Debug.Log("D STARTED");
+        LoadDialogue();
+        npcName.text = NPC.npcDialogue.Name;
+        npcDialogue.text = dialogueQueue.Peek();
+    }
+    private void LoadDialogue()
+    {
+        if (NPC.npcDialogue.Dialogue.Length <= 0) return;
+        foreach (string sentence in NPC.npcDialogue.Dialogue)
+        {
+            dialogueQueue.Enqueue(sentence);
+        }
     }
 
+    private void ContinueDialogue()
+    {
+        if (NPC == null)
+        {
+            dialogueQueue.Clear();
+            return;
+        }
+
+        if (dialogueQueue.Count <= 0)
+        {
+            EndDialogue();
+            dialogueStarted = false;
+            return;
+        }
+
+        npcDialogue.text = dialogueQueue.Dequeue();
+    }
     public void EndDialogue()
     {
+        dialoguePanel.SetActive(false);
+        dialogueQueue.Clear();
         dialogueStarted = false;
     }
 }
--- Assets/Scripts/NPC Man/NPCInteract.cs
+++ Assets/Scripts/NPC Man/NPCInteract.cs
@@ -6,21 +6,22 @@ using UnityEngine.InputSystem;
 
 public class NPCInteract : MonoBehaviour
 {
-    [SerializeField] private GameObject interactionBox;
-    [SerializeField] private TextMeshProUGUI interactionText;
+    [SerializeField] private GameObject interactImage;
+    [SerializeField] private NPCDialogue _npcDialogue;
 
     private bool inRadius = false;
 
+    public NPCDialogue npcDialogue => _npcDialogue;
 
     private void OnEnable()
     {
         InputManager.InputInstance.OnInteractInput += InteractNPC;
     }
 
-    private void Start()
-    {
-        interactionText.text = $"Press {InputManager.InputInstance.PlayerInputActions.Player.Interact.GetBindingDisplayString()} to Talk";
-    }
+    //private void Start()
+    //{
+    //    interactionText.text = $"Press {InputManager.InputInstance.PlayerInputActions.Player.Interact.GetBindingDisplayString()} to Talk";
+    //}
 
     private void InteractNPC()
     {
@@ -35,16 +36,17 @@ public class NPCInteract : MonoBehaviour
 
         DialogueManager.Instance.NPC = this;
         inRadius = true;
-        interactionBox.SetActive(true);
+        interactImage.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) return;
-
-        inRadius = false;
-        DialogueManager.Instance.NPC = null;
-        DialogueManager.Instance.EndDialogue();
-        interactionBox.SetActive(false);
+        if (other.CompareTag("Player"))
+        {
+            inRadius = false;
+            DialogueManager.Instance.NPC = null;
+            DialogueManager.Instance.EndDialogue();
+            interactImage.SetActive(false);
+        }
     }
 }
