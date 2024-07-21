18191d5eaa650d1b14d39afd32d2ce3673a123a0
Casey Mak <casblood18@gmail.com>
2024-07-20 21:36:28 -0400

    Revert "MergeConflict"
    This reverts commit ca188a3d90390cf014ff2efb731149866ed0d124.

--- Assets/Scripts/NPC Man/NPCInteract.cs
+++ Assets/Scripts/NPC Man/NPCInteract.cs
@@ -6,22 +6,21 @@ using UnityEngine.InputSystem;
 
 public class NPCInteract : MonoBehaviour
 {
-    [SerializeField] private GameObject interactImage;
-    [SerializeField] private NPCDialogue _npcDialogue;
+    [SerializeField] private GameObject interactionBox;
+    [SerializeField] private TextMeshProUGUI interactionText;
 
     private bool inRadius = false;
 
-    public NPCDialogue npcDialogue => _npcDialogue;
 
     private void OnEnable()
     {
         InputManager.InputInstance.OnInteractInput += InteractNPC;
     }
 
-    //private void Start()
-    //{
-    //    interactionText.text = $"Press {InputManager.InputInstance.PlayerInputActions.Player.Interact.GetBindingDisplayString()} to Talk";
-    //}
+    private void Start()
+    {
+        interactionText.text = $"Press {InputManager.InputInstance.PlayerInputActions.Player.Interact.GetBindingDisplayString()} to Talk";
+    }
 
     private void InteractNPC()
     {
@@ -36,17 +35,16 @@ public class NPCInteract : MonoBehaviour
 
         DialogueManager.Instance.NPC = this;
         inRadius = true;
-        interactImage.SetActive(true);
+        interactionBox.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {
-            inRadius = false;
-            DialogueManager.Instance.NPC = null;
-            DialogueManager.Instance.EndDialogue();
-            interactImage.SetActive(false);
-        }
+        if (other.CompareTag("Player")) return;
+
+        inRadius = false;
+        DialogueManager.Instance.NPC = null;
+        DialogueManager.Instance.EndDialogue();
+        interactionBox.SetActive(false);
     }
 }
