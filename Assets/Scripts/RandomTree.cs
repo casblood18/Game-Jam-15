de4c97718fdc9c2591f6ee6463cfe5c38811fa4c
XinluChen <lucy.chen991207@gmail.com>
2024-07-30 22:07:57 -0700

    Merge from hell
    fixed bugs in menu and map tree.

---
+++ Assets/Scripts/RandomTree.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTree : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (animator != null)
+        {
+            AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+            float clipLength = clipInfo[0].clip.length;
+
+            float randomStartTime = Random.Range(0f, clipLength);
+
+            animator.Play(clipInfo[0].clip.name, 0, randomStartTime / clipLength);
+        }
+    }
+}
